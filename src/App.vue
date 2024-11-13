04ffeed48a802a8eaae01a339e4b24f4afcf0352
nitikron99 <nitikron.pi@gmail.com>
2024-11-14 03:33:12 +0700

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DashboardPage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DashboardPage from './components/DashboardPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DashboardPage
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
